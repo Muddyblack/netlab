3bd94778b44c73a143efaf8deb724b55e03ac275
Ivan Pepelnjak <ip@ipspace.net>
2026-04-04 12:37:48 +0200

    Split module support tables into groups and multiple tables (#3268)
    The module support tables became way too large. This PR:
    
    * Splits the support tables into multiple tables when the number of
      features exceeds the maximum number of columns (default: 7)
    * Display features in groups when configured in the module 'features'
      dictionary (example: bgp)

--- netsim/cli/show/modules.py
+++ netsim/cli/show/modules.py
@@ -9,9 +9,9 @@ import typing
 from box import Box
 
 from ... import data
-from ...utils import strings
+from ...utils import log, strings
 from .. import error_and_exit
-from .utils import DEVICES_TO_SKIP, get_modlist, parser_add_module, show_common_parser
+from .utils import DEVICES_TO_SKIP, get_modlist, parser_add_module, show_common_parser, split_table
 
 
 def parse() -> argparse.ArgumentParser:
@@ -22,11 +22,20 @@ def parse() -> argparse.ArgumentParser:
     dest='feature',
     action='store',
     help='Display information for a single feature of the selected module')
+  parser.add_argument(
+    '--columns',
+    dest='columns',
+    action='store',
+    default=7,
+    type=int,
+    help='Maximum number of columns in the feature table(s)')
   return parser
 
-def get_feature_list(features: Box,prefix: str = '') -> list:
+def get_feature_list(features: dict,prefix: str = '') -> list:
   f_list = []
   for k in features.keys():
+    if k == '_title':
+      continue
     if isinstance(features[k],dict):
       f_list.extend(get_feature_list(features[k],k+'.'))
     else:
@@ -34,13 +43,27 @@ def get_feature_list(features: Box,prefix: str = '') -> list:
 
   return f_list
 
+def get_module_flat_features(features: Box, prefix: str = '', path: str = '', f_dict: dict = {}) -> dict:
+  for k,v in features.items():
+    if k == '_title':
+      continue
+    if not isinstance(v,Box):
+      f_dict[prefix+k] = {'path': path + k, 'name': v }
+    elif '_title' in v:
+      get_module_flat_features(v,prefix,path+k+'.',f_dict)
+    else:
+      get_module_flat_features(v,prefix+k+'.',path+k+'.',f_dict)
+
+  return f_dict
+
 def device_module_feature_row(
       settings: Box, *,
       rows: list,
       heading: list,
       device: str,
       module: str,
-      provider: typing.Optional[str]) -> bool:
+      provider: typing.Optional[str] = None,
+      add_empty: bool = True) -> bool:
 
   d_data = settings.devices[device]
   if provider:
@@ -78,32 +101,61 @@ def device_module_feature_row(
     value = value.center(len(f))
     row.append(value)
 
-  rows.append(row)
-  return has_feature
+  if has_feature or add_empty:
+    rows.append(row)
+  return has_feature and add_empty
 
-def show_module_features(settings: Box, args: argparse.Namespace,dev_list: list) -> None:
+# The core "show_feature_table" processing displays a table of features
+# supported by netlab platforms. The table is limited to selected features
+# (feature_list), selected devices (dev_list) and potentially uses category-
+# specific feature names (features).
+#
+# The final flag (show_notes) controls whether the "some devices support no
+# extra features" text is printed. The default (true) is used for non-category
+# features and the core category (category with '_title' set to none).
+#
+# Finally, the function splits the table into multiple tables when it has to
+# display more than args.columns features and recursively calls itself to
+# generate the split tables
+#
+def show_feature_table(
+      settings: Box,
+      feature_list: list,
+      args: argparse.Namespace,
+      dev_list: list,
+      features: typing.Optional[dict] = None,
+      show_notes: bool = True) -> None:
   m = args.module
+  if len(feature_list) > args.columns:            # Do we need to generate multiple tables?
+    for cnt,feature_cols in enumerate(split_table(feature_list,args.columns)):
+      if cnt:                                     # Empty line between tables
+        print()
+      show_feature_table(settings,feature_cols,args,dev_list,features,show_notes)
+      show_notes=False                            # Do not show notes after the first table
+    return
+
   heading = ['device']
-  heading.extend(get_feature_list(settings[m].features))
+  heading.extend(feature_list)
   providers = settings.providers.keys()
 
   rows: list = []
   need_notes = False
-
   for d in sorted(dev_list):
     if d in DEVICES_TO_SKIP:
       continue
 
-    if not device_module_feature_row(settings,rows=rows,heading=heading,device=d,module=m,provider=None):
+    if not device_module_feature_row(
+              settings,rows=rows,heading=heading,device=d,module=m,provider=None,add_empty=show_notes):
       need_notes = True
 
     for p_name in providers:
-      if not device_module_feature_row(settings,rows=rows,heading=heading,device=d,module=m,provider=p_name):
+      if not device_module_feature_row(
+              settings,rows=rows,heading=heading,device=d,module=m,provider=p_name,add_empty=show_notes):
         need_notes = True
 
   strings.print_table(heading,rows)
 
-  if need_notes:
+  if need_notes and show_notes:
     print(f"""
 Notes:
 * All devices listed in the table support {m} configuration module.
@@ -111,8 +163,46 @@ Notes:
     
   print("")
   print("Feature legend:")
+  if not features:
+    features = settings[m].features
+  if not features:
+    return
+
   for f in heading[1:]:
-    print(f"* {f}: {settings[m].features[f]}")
+    print(f"* {f}: {features[f]}")
+
+# The top-level "show features table" function splits feature categories from
+# regular features, iterates over categories (printing title and showing category
+# table), and finally prints regular features table.
+#
+# The "show notes" which displays "some devices support no features" note is set
+# to True only when the category title is None (indicating "core" features
+#
+def show_module_features(settings: Box, args: argparse.Namespace,dev_list: list) -> None:
+  m = args.module
+  mod_features = settings[m].features
+  categories = [ cname for cname,cdata in mod_features.items()
+                    if isinstance(cdata,Box) and '_title' in cdata ]
+  features = { k:v for k,v in mod_features.items() if k not in categories }
+
+  for cname in categories:
+    cdata = mod_features[cname]
+    sub_category = bool(cdata.get('_title',None))
+    if sub_category:
+      print("\n")
+      strings.print_colored_text(cdata._title,"bold")
+      print("\n")
+
+    show_feature_table(
+      settings,
+      feature_list=get_feature_list(cdata),
+      args=args,
+      dev_list=dev_list,
+      features=cdata,
+      show_notes=not sub_category)
+
+  if features:
+    show_feature_table(settings,get_feature_list(features),args,dev_list)
 
 def show(settings: Box, args: argparse.Namespace) -> None:
   if args.module == 'initial':
@@ -120,19 +210,31 @@ def show(settings: Box, args: argparse.Namespace) -> None:
   else:
     mod_list = get_modlist(settings,args)
 
+  if args.columns < 2 or args.columns > 20:
+    log.info('The value of --columns flag should be between 2 and 20. Using default value (7)')
+    args.columns = 7
+
   result = data.get_empty_box()
 
+  # When the user specifies the --feature flag, we have to unroll the categories features
+  # into a flat list, use the flat list to check the feature value (or generate a list of
+  # features) and limit the printout to the selected feature
+  #
   if args.feature:
     if not args.module:
+      log.fatal('The --feature flag can be used only with the --module flag')
+    flat_features = get_module_flat_features(settings[args.module].features)
+    if not args.module:
       error_and_exit('The --feature parameter is only valid with the --module parameter')
-    if args.feature not in settings[args.module].features:
+    if args.feature not in flat_features:
+      f_list = [ f"* {k}: {flat_features[k]['name']}" for k in sorted(flat_features.keys())]
       error_and_exit(
         f'Module {args.module} does not have feature {args.feature}',
-        more_hints=f'Use "netlab show defaults {args.module}.features" to display valid device features')
+        more_hints=[f'Valid {args.module} features are:\n']+f_list)
 
     # Remove all other features from the module feature list to display just the selected feature
     #
-    f = settings[args.module].features[args.feature]
+    f = settings[args.module].features[flat_features[args.feature]['path']]
     settings[args.module].features = { args.feature: f }
 
   if args.format == 'table':
